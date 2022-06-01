74c174623fcb34ab06eabb370874f629dd07a35f
kaizerj <kzrster@gmail.com>
2022-06-02 01:42:05 +0800

    BACKEND. FRONTEND. Add new sizes

---
+++ frontend/build/precache-manifest.fea82b567fc01c30a01c85e79bf25db5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6268f5b760a73bce71ea4cd869f28c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "185557b99b82e8d146e6",
+    "url": "/static/js/2.5b87800f.chunk.js"
+  },
+  {
+    "revision": "385801f39fe2615b5790da562b481c6c",
+    "url": "/static/js/2.5b87800f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "628fab18e358c2cbcf27",
+    "url": "/static/js/main.bab77e64.chunk.js"
+  },
+  {
+    "revision": "8bba03c2c12330ef7b9f",
+    "url": "/static/js/runtime-main.5a9097eb.js"
+  }
+]);
